67d35be6ebc2239fbbe7417546d201f3479a3375
Gunnaway <stevengunneweg@hotmail.com>
2014-05-24 15:41:53 +0200

    added icon

--- 1337/index.php
+++ 1337/index.php
@@ -1,6 +1,7 @@
 <!DOCTYPE HTML>
 <html>
 <head>
+	<link rel="shortcut icon" href="assets/favicon.ico"/>
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
 	<script type="text/javascript">
 		window.onload = function() {
