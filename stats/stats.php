908b6d83dd6ba18a7f753b04b94841afedfd26ff
root <root@d1stkfactory>
2014-07-30 15:38:39 +0200

    now tracing edited values for avg time

--- stats/stats.php
+++ stats/stats.php
@@ -53,7 +53,7 @@ function getAvgTime() {
 	$total = 0;
 	$count = 0;
 	while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-		print_r($row['time']);
+		echo(str_replace('.', '', str_replace(':', '', $row['time'])));
 		$total += intval(str_replace('.', '', str_replace(':', '', $row['time'])));
 		$count++;
     }
