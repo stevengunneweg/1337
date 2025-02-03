b1ca2bba7bfe42f6f4e28f100b5e6cbc6a12176c
Steven <steven.gunneweg@newstory.nl>
2025-02-03 10:12:50 +0100

    fix(3.0.1): Add files to remove pre-existing webworker

---
+++ public/v2/service-worker.js
@@ -0,0 +1,39 @@
+// /**
+//  * Welcome to your Workbox-powered service worker!
+//  *
+//  * You'll need to register this file in your web app and you should
+//  * disable HTTP caching for this file too.
+//  * See https://goo.gl/nhQhGp
+//  *
+//  * The rest of the code is auto-generated. Please don't update this file
+//  * directly; instead, make changes to your Workbox build configuration
+//  * and re-run your build process.
+//  * See https://goo.gl/2aRDsh
+//  */
+
+// importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js');
+
+// importScripts('/v2/precache-manifest.a085e8f64ee34e8303188fa612714eed.js');
+
+// workbox.core.setCacheNameDetails({ prefix: '1337online' });
+
+// self.addEventListener('message', (event) => {
+// 	if (event.data && event.data.type === 'SKIP_WAITING') {
+// 		self.skipWaiting();
+// 	}
+// });
+
+// /**
+//  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+//  * requests for URLs in the manifest.
+//  * See https://goo.gl/S9QRab
+//  */
+// self.__precacheManifest = [].concat(self.__precacheManifest || []);
+// workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+navigator.serviceWorker.getRegistrations().then((registrations) => {
+	for (const registration of registrations) {
+		registration.unregister();
+	}
+});
+window.location.reload();
